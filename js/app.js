780611d32486e0e09f5e52f958bef1216a1ecc2a
Ian Scott <ianalexanderplasma@gmail.com>
2020-09-01 12:05:04 -0700

    Removing test data

--- js/app.js
+++ js/app.js
@@ -294,3 +294,36 @@ function convertMonth(month){
   }
   return m;
 }
+
+function draggable(){
+  
+  $('.table-dragable tbody .grabable').mousedown(function (e) {
+    console.log('DRAG');
+    var tr = $(e.target).closest('tr'), sy = e.pageY, drag;
+    if ($(e.target).is('tr')) tr = $(e.target);
+    var index = tr.index();
+    $(tr).addClass('grabbed');
+
+    function move (e) {
+      if (!drag && Math.abs(e.pageY - sy) < 10) return;
+      drag = true;
+      tr.siblings().each(function() {
+        var s = $(this), i = s.index(), y = s.offset().top;
+        if (e.pageY >= y && e.pageY < y + s.outerHeight()) {
+          if (i < tr.index()) s.insertAfter(tr);
+          else s.insertBefore(tr);
+          return false;
+        }
+      });
+    }
+    function up (e) {
+      if (drag && index != tr.index()) {
+        drag = false;
+      }
+      $(document).unbind('mousemove', move).unbind('mouseup', up);
+      $(tr).removeClass('grabbed');
+    }
+    $(document).mousemove(move).mouseup(up);
+    stripe();
+  });
+}
