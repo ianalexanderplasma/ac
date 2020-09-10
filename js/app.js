43069ce70d8b085a16c438200420db682a0d634c
Ian Scott <ianalexanderplasma@gmail.com>
2020-09-10 10:30:49 -0700

    Trying new. date function

--- js/app.js
+++ js/app.js
@@ -47,23 +47,27 @@ $(document).ready(function(){
 
 
 // Function to convert excel dates to JS Date objects
-function ExcelDateToJSDate(serial) {
-  var utc_days  = Math.floor(serial - 25570);
-  var utc_value = utc_days * 86400;                                        
-  var date_info = new Date(utc_value * 1000);
+// function ExcelDateToJSDate(serial) {
+//   var utc_days  = Math.floor(serial - 25569);
+//   var utc_value = utc_days * 86400;                                        
+//   var date_info = new Date(utc_value * 1000);
 
-  var fractional_day = serial - Math.floor(serial) + 0.0000001;
+//   var fractional_day = serial - Math.floor(serial) + 0.0000001;
 
-  var total_seconds = Math.floor(86400 * fractional_day);
+//   var total_seconds = Math.floor(86400 * fractional_day);
 
-  var seconds = total_seconds % 60;
+//   var seconds = total_seconds % 60;
 
-  total_seconds -= seconds;
+//   total_seconds -= seconds;
 
-  var hours = Math.floor(total_seconds / (60 * 60));
-  var minutes = Math.floor(total_seconds / 60) % 60;
+//   var hours = Math.floor(total_seconds / (60 * 60));
+//   var minutes = Math.floor(total_seconds / 60) % 60;
 
-  return new Date(date_info.getFullYear(), date_info.getMonth(), date_info.getDate(), hours, minutes, seconds);
+//   return new Date(date_info.getFullYear(), date_info.getMonth(), date_info.getDate(), hours, minutes, seconds);
+// }
+
+function ExcelDateToJSDate(date) {
+  return new Date(Math.round((date - 25569)*86400*1000));
 }
 
 function symbolize(type, pdm_adm){
